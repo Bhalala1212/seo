27e8489be082c4ac909a51b60e962ae5a249e57f
Bhalala1212 <104699443+Bhalala1212@users.noreply.github.com>
2022-10-09 00:53:25 +0530

    Add files via upload

---
+++ precache-manifest.1d5dca5e1d6ca417aea5e332c7ad1995.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5446c3e9c7e81c929fcef18444cff868",
+    "url": "/index.html"
+  },
+  {
+    "revision": "131e7b7fe52f2ae8044c",
+    "url": "/static/css/main.7f62a5dd.chunk.css"
+  },
+  {
+    "revision": "6d990e6d1cd341ccb903",
+    "url": "/static/js/2.6a7d3a2e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6a7d3a2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "131e7b7fe52f2ae8044c",
+    "url": "/static/js/main.0e135385.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "6dfd9f271d4ab285e2dc72f04b72e4b8",
+    "url": "/static/media/Mediya-01.6dfd9f27.png"
+  },
+  {
+    "revision": "64871349648d1cf0d68dd82135697fb4",
+    "url": "/static/media/avatar.64871349.png"
+  },
+  {
+    "revision": "fc5e16f5115d555596d0722d457f218a",
+    "url": "/static/media/loading.fc5e16f5.gif"
+  }
+]);
